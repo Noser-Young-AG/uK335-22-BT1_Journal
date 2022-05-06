ad7cb377e32c2db39cd0f6b803c0a654e4c4e1ec
Anton <69902586+ant0n7@users.noreply.github.com>
2022-05-06 14:39:25 +0200

    Change props of ReminderCard and add Reminder class.
    Reminder class has all properties required for managing reminders and can
    be used for data transfer and persistence.

--- App.tsx
+++ App.tsx
@@ -2,17 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ReminderCard from './ui/molecules/ReminderCard';
 import Recurrence from './types/Recurrence';
+import { Reminder } from './types/Reminder';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <ReminderCard
-        recurrence={Recurrence.WEEKLY}
-        weekday="Monday"
-        hour={16}
-        minute={30}
-        recurringAmount={"forever"}
+        reminder={
+          new Reminder(
+            Recurrence.WEEKLY,
+            "Monday", 16, 30, 7,
+          )
+        }
+        onEdit={() => console.log("Edit")}
+        onDelete={() => console.log("Delete")} 
       />
       <StatusBar style="auto" />
     </View>
