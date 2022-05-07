45183c6288195b7b14c39264e4056bf5ee7f447a
Anton <69902586+ant0n7@users.noreply.github.com>
2022-05-07 10:59:11 +0200

    Implement Navigaiton between pages.
    Home screen is working -> if no reminder is set, "No reminders set" shows.
    Run `yarn install` to fix imports.
    
    Formatted some code, changed styles.

---
+++ ui/organisms/ReminderCardGroup.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Recurrence from "../../types/Recurrence";
+import { Reminder } from "../../types/Reminder";
+import FabElement from "../atoms/FabElement";
+import TitleElement from "../atoms/TitleElement";
+import ReminderCard from "../molecules/ReminderCard";
+
+type ReminderCardGroupProps = {
+  reminder?: Reminder;
+  onEdit: () => void;
+  onDelete: () => void;
+  onCreate: () => void;
+};
+
+function ReminderCardGroup(props: ReminderCardGroupProps) {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+  
+  if (props.reminder == null) {
+    return (
+      <View style={styles.container}>
+        <TitleElement name="No reminders set" />
+        <FabElement
+          name="Create"
+          hidden={false}
+          onPress={props.onCreate}
+        />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <ReminderCard
+          reminder={props.reminder!}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+        />
+      </View>
+    );
+  }
+}
+
+export default ReminderCardGroup;
