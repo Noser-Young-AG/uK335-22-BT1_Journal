83bb337cc04ad81f75206032eec689aa93f14880
Anton <69902586+ant0n7@users.noreply.github.com>
2022-05-07 11:00:41 +0200

    Implement Navigation (right files)
    See last commit.

--- ui/atoms/TitleElement.tsx
+++ ui/atoms/TitleElement.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-export default function TitleElement(props: {name: string}) {
-  return (
-    <View style={styles.container}>
-    <Text style= {styles.text}>{props.name}</Text>
-    </View>
-  )
+function TitleElement(props: { name: string }) {
+  return <Text style={styles.text}>{props.name}</Text>;
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#fff',
-    },
-    text: {
-        fontWeight: "bold", 
-        fontSize: 24
-    }
-  });
+  container: {
+    flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontWeight: "200",
+    fontSize: 24,
+    textTransform: "uppercase",
+  },
+});
+
+export default TitleElement
