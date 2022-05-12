0f1b9653e78aeabcacb3d3a57e4179dfb519accb
xRemo7 <aeberliremo@gmail.com>
2022-05-12 13:38:44 +0200

    formatted files

--- ui/organisms/Navbar.tsx
+++ ui/organisms/Navbar.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Appbar } from 'react-native-paper'
+import React from "react";
+import { Appbar } from "react-native-paper";
 
 type NavbarProps = {
-    title: string
-}
+  title: string;
+};
 
 export default function Navbar(props: NavbarProps) {
   return (
     <Appbar.Header>
-        <Appbar.Content title={props.title}></Appbar.Content>
+      <Appbar.Content title={props.title}></Appbar.Content>
     </Appbar.Header>
-  )
+  );
 }
